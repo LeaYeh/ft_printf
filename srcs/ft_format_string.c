e3b761ba141c698c52a28a128bd2a3b91f70c04f
LeaYeh <smile2140@gmail.com>
2023-09-15 21:45:05 +0200

    [fix] Calculate total len by width and perc_len in formatter string

--- srcs/ft_format_string.c
+++ srcs/ft_format_string.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 16:01:22 by lyeh              #+#    #+#             */
-/*   Updated: 2023/09/15 18:42:27 by lyeh             ###   ########.fr       */
+/*   Updated: 2023/09/15 21:43:43 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ char	*ft_format_string(char *s, t_print_tab *tab)
 
 	if (!s)
 		s = "(null)";
-	tab->total_len = tab->width;
-	if (!tab->width)
+	if (tab->width)
+		tab->total_len = ft_max(ft_strlen(s), tab->width);
+	else if (tab->perc_len)
 		tab->total_len = ft_min(ft_strlen(s), tab->perc_len);
-	tmp = ft_substr(s, 0, tab->perc_len);
+	else
+		tab->total_len = ft_strlen(s);
+	tmp = ft_substr(s, 0, tab->total_len);
 	ret = ft_format_padding(tmp, pad_char, tab->total_len, tab->f_dash);
 	free(tmp);
 	return (ret);
