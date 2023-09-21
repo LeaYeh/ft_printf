540eaa1ae41f632f55e10421bed3210f30fc0b5c
Lea Yeh <lyeh@c2r2p6.42vienna.com>
2023-09-21 17:26:17 +0200

    [fix] Pad zero by width only if not perc_fmt

--- srcs/ft_format_uint.c
+++ srcs/ft_format_uint.c
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 22:58:27 by lyeh              #+#    #+#             */
-/*   Updated: 2023/09/19 17:23:17 by lyeh             ###   ########.fr       */
+/*   Updated: 2023/09/21 17:24:38 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_format_uint(unsigned int n, t_print_tab *tab)
 	char	*formatted_perc;
 
 	pad_char = ' ';
-	if (tab->f_zero_pad)
+	if (tab->f_zero_pad && !tab->f_perc_fmt)
 		pad_char = '0';
 	num_str = ft_utoa(n);
 	formatted_perc = ft_format_persicion(num_str, tab);
